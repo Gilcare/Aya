b004a58967c8fd7d926cf1b9ad990a5baeeade89
Gilcare <gilcare.com@gmail.com>
2026-07-07 22:59:43 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+from database import patient_data
+
+st.set_page_config(
+    page_title="Aya",
+    page_icon="🫁",
+    layout="wide"
+)
+
+st.title("🫁 Aya")
+
+records = list(patient_data.find({}, {"_id":0}))
+
+df = pd.DataFrame(records)
+
+col1,col2,col3 = st.columns(3)
+
+col1.metric(
+    "Total Patients",
+    len(df)
+)
+
+if len(df):
+    col2.metric(
+        "Average Age",
+        round(df.age.mean())
+    )
+
+    col3.metric(
+        "Diagnoses",
+        df.diagnosis.nunique()
+    )
+
+st.divider()
+
+st.subheader("Recent Patients")
+
+st.dataframe(
+    df.tail(10),
+    use_container_width=True,
+    hide_index=True
+)
